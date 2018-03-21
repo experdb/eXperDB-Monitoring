61d262d4b5312b09d58d8f275dfd977af356ede5
goldnoh <goldnoh@k4m.com>
2018-03-21 18:33:03 +0900

    Modified many forms design9

---
+++ eXperDB_Monitoring_Client/iDast.MonPostgres/frmSplashScreen.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSplashScreen))
+        Me.Version = New System.Windows.Forms.Label()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Version
+        '
+        Me.Version.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Version.BackColor = System.Drawing.Color.White
+        Me.Version.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Version.ForeColor = System.Drawing.Color.SlateGray
+        Me.Version.Location = New System.Drawing.Point(254, 5)
+        Me.Version.Name = "Version"
+        Me.Version.Size = New System.Drawing.Size(100, 20)
+        Me.Version.TabIndex = 1
+        Me.Version.Text = "Version {0}.{1:0}"
+        Me.Version.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(360, 130)
+        Me.ShapeContainer1.TabIndex = 4
+        Me.ShapeContainer1.TabStop = False
+        '
+        'RectangleShape1
+        '
+        Me.RectangleShape1.BorderColor = System.Drawing.Color.Silver
+        Me.RectangleShape1.BorderWidth = 3
+        Me.RectangleShape1.Location = New System.Drawing.Point(0, 0)
+        Me.RectangleShape1.Name = "RectangleShape1"
+        Me.RectangleShape1.Size = New System.Drawing.Size(359, 129)
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(358, 128)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'SplashScreen1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(360, 130)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Version)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "SplashScreen1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.TransparencyKey = System.Drawing.Color.Black
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Version As System.Windows.Forms.Label
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents RectangleShape1 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ eXperDB_Monitoring_Client/iDast.MonPostgres/frmSplashScreen.vb
@@ -0,0 +1,39 @@
+﻿Public NotInheritable Class frmSplashScreen
+
+    'TODO: 프로젝트 디자이너에서 "프로젝트" 메뉴의 "속성"을 선택하여 표시된 "응용 프로그램" 탭에서
+    '  이 폼을 응용 프로그램의 시작 화면으로 쉽게 설정할 수 있습니다.
+
+
+    Private Sub SplashScreen1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        '응용 프로그램의 어셈블리 정보에 따라 런타임에 대화 상자 텍스트를 설정합니다.  
+
+        'TODO: "프로젝트" 메뉴에서 선택하여 표시된 프로젝트 속성 대화 상자의 "응용 프로그램" 창에서 응용 프로그램의
+        '  어셈블리 정보를 사용자 지정합니다.
+
+        '응용 프로그램 제목
+        'If My.Application.Info.Title <> "" Then
+        '    ApplicationTitle.Text = My.Application.Info.Title
+        'Else
+        '    '응용 프로그램 제목이 없는 경우 확장명 없이 응용 프로그램 이름을 사용합니다.
+        '    ApplicationTitle.Text = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        'End If
+
+        '디자인 타임에 버전 제어에 설정된 텍스트를 서식 지정 문자열로 사용하여 버전 정보의 서식을
+        '  지정합니다. 이렇게 하면 필요한 경우 효과적인 지역화가 가능합니다.
+        '  다음 코드를 사용하고 버전 제어의 디자인 타임 텍스트를 
+        '  "Version {0}.{1:00}.{2}.{3}" 또는 이와 비슷한 형식으로 변경하여 빌드 정보와 수정 정보를 포함할 수 있습니다.
+        '  자세한 내용은 도움말의 String.Format()을 참조하십시오.
+        '
+        '    Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
+
+        Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor)
+
+        '저작권 정보
+        'Copyright.Text = My.Application.Info.Copyright
+
+        'Me.TransparencyKey = Color.Black
+        'Me.BackColor = Color.Black
+
+    End Sub
+
+End Class
