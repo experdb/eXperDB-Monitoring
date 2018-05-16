ccecf24b6e736454f223408c8a11bb365ce462a1
kayform <jhbaek@k4m.com>
2018-05-16 17:05:44 +0900

    innotsetup

--- eXperDB_Monitoring_Client/DX_Mon_Postgres_InnoSetup/stage/ODBC Drivers/TIBERO_ODBC/win32/ODBCRun.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set mypath="%~dp0"
-
-set mypath=%mypath:"=%\
-set mypath="%mypath:\\=%"
-tbodbc_driver_installer_5_32.exe -i %mypath%
--- eXperDB_Monitoring_Client/DX_Mon_Postgres_InnoSetup/stage/ODBC Drivers/TIBERO_ODBC/win64/ODBCRun.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set mypath="%~dp0"
-
-set mypath=%mypath:"=%\
-set mypath="%mypath:\\=%"
-tbodbc_driver_installer_5_64.exe -i %mypath%
