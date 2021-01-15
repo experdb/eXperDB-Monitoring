ea2f176b4c6954cd0ccf2b63d6d70a330e42e09f
kayform <jhbaek@k4m.com>
2021-01-15 15:28:48 +0900

    missing build-all.bat

--- build-all.bat
+++ build-all.bat
@@ -6,7 +6,7 @@ SET "PATH=%PATH%;%PROGRAMFILES%\Git\bin;C:\apache-ant-1.9.9-bin;C:\Program Files
 
 For /F %%g in ('git rev-parse HEAD ^| cut -b 1-7') Do (Set GIT_COMMIT_HASH=%%g)
 
-set BASE_VER=11.7.1
+set BASE_VER=11.7.3
 set BASE_VER_UDERSCORE=11_7_1
 For /F %%i in ('git rev-list HEAD ^| find /c /v ""') Do Set GIT_COMMIT_CNT=%%i
 
