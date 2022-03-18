2c75e2f9b24c51e853752a5adf63c4de3d1b7955
robin <robin@inzent.com>
2022-03-18 18:41:22 +0900

    [no issue] type : fix Changes - change default password from password to experdb12# - add kstart option for k8s ( try to connect to repository until to ready the repository) Fixes

--- eXperDB_Server/install/eXperDB_Server/experdbmon.sh
+++ eXperDB_Server/install/eXperDB_Server/experdbmon.sh
@@ -4,10 +4,29 @@ MANAGERPATH=eXperDBMA_MANAGER
 SERVERPATH=eXperDBMA
 MANAGERBIN=${MANAGERPATH}/bin
 SERVERBIN=${SERVERPATH}/bin
+MANAGERCONF=${MANAGERPATH}/config
+SERVERCONF=${SERVERPATH}/config
 
 ARGV="$@"
 
 case "$ARGV" in
+# for k8s
+    kstart)
+        if [ ! -z ${EXPERDBREPO} ] ; then
+                sed -i "s/127.0.0.1/${EXPERDBREPO}/g" $SERVERCONF/MyBatisConfig.xml
+                sed -i "s/127.0.0.1/${EXPERDBREPO}/g" $MANAGERCONF/MyBatisConfig.xml
+        fi
+        for ((i=0;i<=5;i++))
+        do
+                ${SERVERBIN}/experdbma.sh restart
+                ${MANAGERBIN}/experdbma.sh restart
+                sleep 20
+                ISRUN=`${SERVERBIN}/experdbma.sh now|wc -l`
+                if [ $ISRUN -gt 0 ]; then
+                        break;
+                fi
+        done
+    ;;
     start|stop|restart|now)
         ${SERVERBIN}/experdbma.sh $@
         ${MANAGERBIN}/experdbma.sh $@
